70debbf092b289e800f08a9dd5842293d624a83a
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-03-19 21:07:43 +0800

    Initialize project
    This commit initializes the project (in regards of the project files).
    A new Xcode project has been created and some adjustments to the project
    details have been made.
    
    Future commits will initialize the project for GitHub—in other words,
    essential documents like README, CODE_OF_CONDUCT, and more will be added
    soon.

---
+++ Liste/Liste/Screens/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Liste
+//
+//  Created by Arash Nur Iman on 01/10/19.
+//  Copyright © 2019 Apprendre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
