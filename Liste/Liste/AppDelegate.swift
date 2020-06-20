245a88eab705886bea2e9913fbf91f8f7bd09193
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-06-20 13:11:47 +0800

    Add conditional navigation
    This commit adds conditional navigation in AppDelegate and SceneDelegate
    that changes where the app starts based on the status of the current
    user (whether they are signed in or out).

--- Liste/Liste/AppDelegate.swift
+++ Liste/Liste/AppDelegate.swift
@@ -11,11 +11,27 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Configures the Firebase library.
         FirebaseApp.configure()
+        
+        // Performs conditional navigation on the condition of currentUser's status.
+        let user = Auth.auth().currentUser
+        if user != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let rootViewController = storyboard.instantiateViewController(identifier: "View")
+            window?.rootViewController = rootViewController
+            window?.makeKeyAndVisible()
+        } else {
+            let storyboard = UIStoryboard(name: "Auth", bundle: nil)
+            let rootViewController = storyboard.instantiateViewController(identifier: "Welcome")
+            window?.rootViewController = rootViewController
+            window?.makeKeyAndVisible()
+        }
         
         return true
     }
