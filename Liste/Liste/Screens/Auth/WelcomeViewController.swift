41abbd2efbfb87c5e2b462902bad5c679f6681de
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-06-20 13:11:47 +0800

    Add conditional navigation
    This commit adds conditional navigation in AppDelegate and SceneDelegate
    that changes where the app starts based on the status of the current
    user (whether they are signed in or out).

---
+++ Liste/Liste/Screens/Auth/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  Liste
+//
+//  Created by Arash Nur Iman on 20/06/20.
+//  Copyright © 2020 Apprendre. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
