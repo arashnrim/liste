29cf44b539ca30eb416f5dedc282df5443d051eb
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-06-21 12:18:19 +0800

    Extend app support
    This commit extends app support to older versions before iOS 13. Future
    commits should ensure that app support is sufficient and precautions are
    in place.

--- Liste/Liste/SceneDelegate.swift
+++ Liste/Liste/SceneDelegate.swift
@@ -9,10 +9,10 @@
 import UIKit
 import Firebase
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
