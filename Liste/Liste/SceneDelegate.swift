b38c042dd9fd834d02ea0ba0869b0e014fa0fcc4
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-06-27 20:44:58 +0800

    Apply reindentation
    This commit performs code reindentation for all current Swift files.

--- Liste/Liste/SceneDelegate.swift
+++ Liste/Liste/SceneDelegate.swift
@@ -11,9 +11,9 @@ import Firebase
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -34,5 +34,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window?.makeKeyAndVisible()
         }
     }
-
+    
 }
