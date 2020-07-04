914e51a7407be4ea56d0864f35a6fa9ac306b680
Arash Nur Iman <47273556+ArashNrIm@users.noreply.github.com>
2020-07-04 09:21:37 +0800

    Fix incompatibility issue
    This commit fixes an incompatibility issue caused by using the Xcode 12
    beta. This commit also adds TaskTableViewCell, which will be a custom
    table view cell used for displaying the user's tasks.

---
+++ Liste/Liste/Screens/Main/TaskTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TaskTableViewCell.swift
+//  Liste
+//
+//  Created by Arash Nur Iman on 30/06/20.
+//  Copyright © 2020 Apprendre. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var taskStatusButton: UIButton!
+    @IBOutlet weak var taskBackgroundView: UIView!
+    @IBOutlet weak var shadowBackgroundView: UIView!
+    @IBOutlet weak var taskTitleLabel: UILabel!
+    @IBOutlet weak var taskDueLabel: UILabel!
+
+}
+
+
