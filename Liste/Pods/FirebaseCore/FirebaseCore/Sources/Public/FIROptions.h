a37ae7933aadf67f633aaf40750792b67639fa98
Arash <47273556+arashnrim@users.noreply.github.com>
2020-07-21 18:33:21 +0800

    Redact secrets
    This commit redacts a secret (although just an example).

--- Liste/Pods/FirebaseCore/FirebaseCore/Sources/Public/FIROptions.h
+++ Liste/Pods/FirebaseCore/FirebaseCore/Sources/Public/FIROptions.h
@@ -32,7 +32,7 @@ NS_SWIFT_NAME(FirebaseOptions)
 
 /**
  * An iOS API key used for authenticating requests from your app, e.g.
- * @"AIzaSyDdVgKwhZl0sTTTLZ7iTmt1r3N2cJLnaDk", used to identify your app to Google servers.
+ * @"---------------------------------------", used to identify your app to Google servers.
  */
 @property(nonatomic, copy, nullable) NSString *APIKey NS_SWIFT_NAME(apiKey);
 
