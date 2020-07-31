69601d0be9641ee2602c13a312aa527fa4708140
Arash <47273556+arashnrim@users.noreply.github.com>
2020-07-31 19:29:13 +0800

    Add AddViewController
    This commit adds a new View Controller screen, AddViewController. This
    View Controller will manage the addition of the user's tasks.
    
    Currently, this commit only adds the View Controller. No design or code
    has been added.

---
+++ Liste/Liste/Screens/Main/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Liste
+//
+//  Created by Arash on 31/07/20.
+//  Copyright © 2020 Apprendre. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
