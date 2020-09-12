63178a9a0e11f525b8c2bff91f4a2ef6f3778ae4
Arash <47273556+arashnrim@users.noreply.github.com>
2020-09-12 16:15:06 +0800

    🎨 Improve code structure
    This commit makes some changes to the project's internal code structure;
    here, function declarations are isolated from their files to make
    development easier.

--- Liste/Liste/Screens/Main/AddViewController.swift
+++ Liste/Liste/Screens/Main/AddViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 import Firebase
 
+/**
+ * All code regarding UI elements of the Add View Controller resides here.
+ *
+ * To view the declaration of code functions interacting with the database, view `AddViewController+Database.swift`.
+ *
+ * - See also: AddViewController+Database.swift
+ */
 class AddViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     // MARK: Outlets
@@ -56,75 +63,6 @@ class AddViewController: UIViewController, UITextFieldDelegate, UITextViewDelega
         if textView.text.isEmpty {
             textView.text = "Task description"
             textView.textColor = UIColor.lightGray
-        }
-    }
-
-    // MARK: Functions
-    /**
-     * Verifies if the user's inputs are valid.
-     *
-     * This function checks only the fundementally important part of a task: its name. This function checks if `taskName` is empty (either an empty string or simply empty). If so, then the check returns a `false`. Otherwise, a check returns a `true`.
-     *
-     * - Returns: A boolean value of the outcome of the check.
-     */
-    func validateInputs() -> Bool {
-        guard let taskName = taskNameField.text else {
-            print("Warning: Something happened when retrieving the value for taskName.")
-            return false
-        }
-
-        return !(taskName.isEmpty || taskName == "")
-    }
-
-    /**
-     * Prepares and compiles all the information given into a `Task`.
-     *
-     * This function reads all the data provided and filled by the user, and afterwards compiles them in the `Task` type.
-     *
-     * - Returns: A `Task` of the compiled data.
-     */
-    func prepareTask() -> Task {
-        guard let taskName = taskNameField.text else {
-            print("Warning: Something happened when retrieving the value for taskName.")
-            return Task(taskName: "", dueDate: Date(), completionStatus: false)
-        }
-        let description = taskDescriptionView.text
-        let dueDate = taskDuePicker.date
-        let task: Task
-
-        if !(description == "Task description") {
-            task = Task(taskName: taskName, dueDate: dueDate, description: description!, completionStatus: false)
-        } else {
-            task = Task(taskName: taskName, dueDate: dueDate, completionStatus: false)
-        }
-        return task
-    }
-
-    /**
-     * Updates the user's tasks on the database.
-     *
-     * This function contacts the Firestore database and sets the data of the user's `tasks` key. This key should be inclusive of the user's previous tasks; failing to do so may result in the user's previous tasks being overwritten.
-     *
-     * This function also assumes that the tasks are converted to the Firestore JSON form of `[String: [String: Any]]`. Use the `convertTaskToJSON` function to assist the conversion.
-     *
-     * - Parameters:
-     *      - tasks: The user's tasks, in the Firestore JSON form of `[String: [String: Any]]`.
-     *      - completion: A closure for code to run following a successful update.
-     */
-    func updateDatabase(tasks: [String: [String: Any]], completion: @escaping (() -> Void)) {
-        guard let userID = Auth.auth().currentUser?.uid else {
-            print("Warning: No authenticatd user is found; future functions may fail.")
-            return
-        }
-        let database = Firestore.firestore()
-
-        database.document("users/\(userID)").updateData(["tasks": tasks]) { (error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                self.displayAlert(title: "An error occurred.", message: error.localizedDescription, override: nil)
-            } else {
-                completion()
-            }
         }
     }
 
