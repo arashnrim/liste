3cca945aa69363b8946d61afdcc4ad0121e6839b
Arash <47273556+arashnrim@users.noreply.github.com>
2020-10-24 18:09:35 +0800

    🚧 Add basic account page
    This commit adds the basic foundation design for AccountViewController.
    Currently, it includes basic details of the user's account that are the
    most important, such as the user's account ID, e-mail address, and the
    number of tasks. There's also a button to delete all data (tasks) and
    the account; a future to-do!

---
+++ Liste/Screens/Settings/AccountViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AccountViewController.swift
+//  Liste
+//
+//  Created by Arash on 24/10/20.
+//  Copyright © 2020 Apprendre. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AccountViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var accountIDLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tasksLabel: UILabel!
+
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Pulls all the required data and populates the labels.
+        if let currentUser = Auth.auth().currentUser {
+            self.accountIDLabel.text = currentUser.uid
+            self.emailLabel.text = currentUser.email
+        }
+
+        var tasks: [Task] = []
+        let firestore = Firestore.firestore()
+        guard let userID = Auth.auth().currentUser?.uid else {
+            print("Warning: No authenticated user is found; future functions may fail.")
+            return
+        }
+        firestore.document("users/\(userID)").getDocument { (snapshot, error) in
+            if let error = error {
+                print("Error (while retrieving user data): \(error.localizedDescription)")
+            } else {
+                if let snapshot = snapshot {
+                    let data = snapshot.data()
+                    let databaseTasks = data!["tasks"] as! [String: [String: Any]]
+                    tasks = self.convertJSONToTask(tasks: databaseTasks)
+                    self.tasksLabel.text = String(tasks.count)
+                }
+            }
+        }
+
+    }
+}
