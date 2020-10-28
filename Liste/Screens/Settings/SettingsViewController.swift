6335f407d583262c2dcba745f877eadbf3570080
Arash <47273556+arashnrim@users.noreply.github.com>
2020-10-28 18:42:07 +0800

    ✨ Add sign out feature
    This commit adds an option for the user to sign out. Why didn't I
    implement this earlier?

--- Liste/Screens/Settings/SettingsViewController.swift
+++ Liste/Screens/Settings/SettingsViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
 
     // MARK: Actions
     @IBAction func dismissButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func signOutButton(_ sender: Any) {
+        self.displayAlert(title: NSLocalizedString("signOutTitle", comment: "Are you sure you'd like to sign out?"), message: NSLocalizedString("signOutMessage", comment: "Your session will be reset and you'll need to sign in again.")) { (alert) in
+            alert.addAction(UIAlertAction(title: NSLocalizedString("signOut", comment: "Sign out."), style: .destructive, handler: { (_) in
+                do {
+                    try Auth.auth().signOut()
+                } catch let error {
+                    print("Error (while signing out): \(error.localizedDescription)")
+                    self.displayAlert(title: NSLocalizedString("errorOccurred", comment: "An error occurred."), message: error.localizedDescription, override: nil)
+                }
+            }))
+            alert.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: "Cancel an action."), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
 }
