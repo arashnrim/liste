03386a6d0c1f7afe5b03249a3edd3ac936858585
Arash <47273556+arashnrim@users.noreply.github.com>
2020-10-29 13:50:42 +0800

    📱 Improve responsive design
    This commit improves the responsive design of the app in the
    re-authentication screen by enabling the keyboard to dismiss on tap and
    press of the 'return' key.

--- Liste/Screens/Settings/ReauthenticationViewController.swift
+++ Liste/Screens/Settings/ReauthenticationViewController.swift
@@ -10,18 +10,73 @@ import UIKit
 import Hero
 import Firebase
 
-class ReauthenticationViewController: UIViewController {
+class ReauthenticationViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Outlets
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var deleteButton: ListeButton!
 
     // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+
+        // Assumes deleteButton to be disabled when the view loads.
+        deleteButton.changeState(state: false, text: "...")
+
+        // Allows editing to end when any part of the screen is tapped outside the keyboard area.
+        self.dismissKeyboardOnTap(completion: nil)
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "signOut" {
             let destination = segue.destination
             destination.hero.modalAnimationType = .zoomOut
         }
+    }
+
+    // MARK: Text Field Protocols
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let valid = validate()
+        valid ? deleteButton.changeState(state: true, text: NSLocalizedString("deleteAccount", comment: "Delete account.")) : deleteButton.changeState(state: false, text: "...")
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case emailTextField:
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            self.view.endEditing(true)
+        default:
+            break
+        }
+        return false
+    }
+
+    // MARK: Functions
+    /**
+     * Checks if the user input in the `emailTextField` and `passwordTextField` fields are valid.
+     *
+     * This function checks if the value in `emailTextField` is not empty and if it contains the "@" symbol. It also checks if the value in `passwordTextField` is not empty and has a character count of more than five; if both of these statements hold true, then the result is successful. Else, the result is failure.
+     *
+     * - Returns: A boolean value of the outcome of the check.
+     */
+    func validate() -> Bool {
+        guard let email = emailTextField.text else {
+            print("Warning: Email String value appears to be empty; assuming the check result to be false.")
+            return false
+        }
+        guard let password = passwordTextField.text else {
+            print("Warning: Password String value appears to be empty; assuming the check result to be false.")
+            return false
+        }
+        let emailValid = !(email.isEmpty) && (email.contains("@"))
+        let passwordValid = !(password.isEmpty) && (password.count > 5)
+
+        return emailValid && passwordValid
     }
 
     // MARK: Actions
